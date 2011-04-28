d18b4f8b826a3ba0beb4e370d06b042109e73c18
Adam Bengtsson <adam.bengtsson@gmail.com>
2011-04-28 16:20:37 +0200

    Draw lines, directions

--- src/org/pathpal/GMapsActivity.java
+++ src/org/pathpal/GMapsActivity.java
@@ -1,20 +1,36 @@
 package org.pathpal;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.amelie.driving.DrivingDirections;
+import com.amelie.driving.Placemark;
+import com.amelie.driving.Route;
+import com.amelie.driving.DrivingDirections.IDirectionsListener;
+import com.amelie.driving.DrivingDirections.Mode;
+import com.amelie.driving.impl.DrivingDirectionsGoogleKML;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
+import com.google.android.maps.Projection;
 
-public class GMapsActivity extends MapActivity {
+public class GMapsActivity extends MapActivity implements IDirectionsListener {
 
 	private MapView mapView;
+	private Projection projection;
+	private CustomItemizedOverlay itemizedOverlay;
 	
 	private static final int latitudeE6 = 37985339;
 	private static final int longitudeE6= 23716735;
@@ -29,19 +45,33 @@ public class GMapsActivity extends MapActivity {
 		mapView.setBuiltInZoomControls(true);
 		
 		List<Overlay> mapOverlays = mapView.getOverlays();
-		Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
-		CustomItemizedOverlay itemizedOverlay = new CustomItemizedOverlay(drawable, this);
+		projection = mapView.getProjection();
+		
 		
 		GeoPoint point = new GeoPoint(latitudeE6, longitudeE6);
+		/*
+		Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+		itemizedOverlay = new CustomItemizedOverlay(drawable, this);
+		
+		
 		OverlayItem overlayitem = new OverlayItem(point, "hello", "im in athens, greece");
 		
 		itemizedOverlay.addOverlay(overlayitem);
-		mapOverlays.add(itemizedOverlay);
-		
+		mapOverlays.add(itemizedOverlay); */
+//      mapOverlays.add(new PathOverlay());
 		MapController mapController = mapView.getController();
 		
 		mapController.animateTo(point);
-		mapController.setZoom(6);
+		
+		
+		DrivingDirections dd = new DrivingDirectionsGoogleKML();
+		GeoPoint gp          = new GeoPoint(37985339, 23716735);
+		GeoPoint gp2         = new GeoPoint(38036160, 23787610);
+		
+		dd.driveTo(gp, gp2, Mode.DRIVING, this);
+		
+		mapController.animateTo(point);
+		mapController.setZoom(10);
 		
 	}
 	
@@ -49,6 +79,75 @@ public class GMapsActivity extends MapActivity {
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	public void onDirectionsAvailable(Route route, Mode mode) {
+		List<GeoPoint> gps = route.getGeoPoints();
+//		List<Placemark> pl = route.getPlacemarks();
+	
+		List<Overlay> mapOverlays = mapView.getOverlays();
+/*		Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+		CustomItemizedOverlay itemizedOverlay = new CustomItemizedOverlay(drawable, this);
+		
+*/
+		Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+		itemizedOverlay = new CustomItemizedOverlay(drawable, this);
+		
+		GeoPoint point = new GeoPoint(latitudeE6, longitudeE6);
+		OverlayItem overlayitem = new OverlayItem(point, "hello", gps.size()+"");
+		
+		itemizedOverlay.addOverlay(overlayitem);
+		mapOverlays.add(itemizedOverlay);
+		
+		mapOverlays.add(new PathOverlay(gps));
+		
+	}
+
+	public void onDirectionsNotAvailable() {
+		// TODO Auto-generated method stub
+		throw new RuntimeException();
+	}
+	
+	public class PathOverlay extends Overlay {
+		
+		private GeoPoint first;
+		private List<GeoPoint> points;
+		//Projection 	private Projection projection;
+		
+	    public PathOverlay(List<GeoPoint> points) {
+	    	this.points = new ArrayList<GeoPoint>(points);
+	    	this.first = this.points.remove(0);
+	    }
+
+	    public void draw(Canvas canvas, MapView mapv, boolean shadow){
+	    	
+	        super.draw(canvas, mapv, shadow);
+	        Paint mPaint = new Paint();
+	        mPaint.setDither(true);
+	        mPaint.setColor(Color.RED);
+	        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+	        mPaint.setStrokeJoin(Paint.Join.ROUND);
+	        mPaint.setStrokeCap(Paint.Cap.ROUND);
+	        mPaint.setStrokeWidth(2);
+	        
+	        Path path = new Path();
+	        
+	        GeoPoint from = this.first;
+	        for (GeoPoint to : this.points) {
+	        	Point p1 = new Point();
+		        Point p2 = new Point();
+		        
+		        projection.toPixels(from, p1);
+		        projection.toPixels(to, p2);
+
+		        path.moveTo(p2.x, p2.y);
+		        path.lineTo(p1.x,p1.y);
+		        from = to;
+	        }
+	        
+	        canvas.drawPath(path, mPaint);
+	    }
+	    
 	}
 
 }
