404713adffb33bff1d0ab62db6a60497e0454c8d
Mikael Bung <phyrex1an@hotmail.com>
2011-05-05 16:19:27 +0200

    Changed TranslatorApi to take pgf file as a stream instead of a file name

--- src/org/pathpal/TranslatorApi.java
+++ src/org/pathpal/TranslatorApi.java
@@ -2,14 +2,15 @@ package org.pathpal;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.grammaticalframework.*;
 import org.grammaticalframework.Trees.Absyn.*;
 
 public class TranslatorApi {
-	public static boolean translateString(String inputString, DirectionsForm form) throws FileNotFoundException, IOException, UnknownLanguageException {
+	public static boolean translateString(String inputString, DirectionsForm form, InputStream pgffile) throws FileNotFoundException, IOException, UnknownLanguageException {
 		String[] words = inputString.split(" ");
-		PGF pgf = PGFBuilder.fromFile("locator.pgf");
+		PGF pgf = PGFBuilder.fromInputStream(pgffile);
 		Parser parser = new Parser(pgf, "LocatorSwe");
 		Tree[] trees = (Tree[]) parser.parse(words).getTrees();
 		if(trees.length < 1) { return false; }
