640c1889c48cb3d01a0b53db916262a6f2f3d1cd
Adam Bengtsson <adam.bengtsson@gmail.com>
2011-05-06 17:36:31 +0200

    MyLocationWaypoint added

---
+++ src/org/pathpal/MyLocationWaypoint.java
@@ -0,0 +1,27 @@
+package org.pathpal;
+
+import java.io.IOException;
+
+import org.pathpal.DirectionsForm.Waypoint;
+
+import com.google.android.maps.GeoPoint;
+
+import android.location.Address;
+
+public class MyLocationWaypoint implements Waypoint {
+	
+	GeoPoint p;
+	public MyLocationWaypoint(GeoPoint p){
+		this.p = p;
+	}
+
+	public Address findAddress(SearchApi api) throws IOException {
+		Address a = api.geocoder.getFromLocation(fromE6(p.getLatitudeE6()) , fromE6(p.getLongitudeE6()), 1).remove(0);
+		return a;
+	}
+	
+	private double fromE6(int l){
+		return (((double)l) / 1000000);
+	}
+
+}
